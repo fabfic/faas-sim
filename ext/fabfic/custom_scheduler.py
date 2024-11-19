3e937bbffc15f0c780d756b6ed5beb630e132100
fabfic <fabriziofichera@proton.me>
2024-11-19 19:31:39 +0000

    Added predicates for clopu/edge scheduling policy

--- ext/fabfic/custom_scheduler.py
+++ ext/fabfic/custom_scheduler.py
@@ -1,6 +1,8 @@
 import logging
 from typing import List, Tuple
 
+from skippy.core.predicates import Predicate, PodFitsResourcesPred, CheckNodeLabelPresencePred
+from ext.fabfic.predicates import CheckCloudNodePred, CheckEdgeNodePred
 from skippy.core.scheduler import Scheduler
 from skippy.core.priorities import Priority, BalancedResourcePriority, \
     LatencyAwareImageLocalityPriority, CapabilityPriority, DataLocalityPriority
@@ -13,7 +15,7 @@ from sim.faassim import Simulation
 logger = logging.getLogger(__name__)
 
 #Set this flag true to schedule functions in the cloud
-cloud = False
+consider_cloud_nodes = False
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
@@ -29,6 +31,12 @@ def main():
 
 
 class CustomScheduler:
+    '''
+    This scheduler puts into consideration the locality of the node 
+    by implementig  a cloud/edge locality scheduling policy.
+    Use priorities to affect the scoring of nodes during scheduling.
+    Use predicates to completely remove cloud/edge nodes from scheduling.
+    '''
 
     @staticmethod
     def create(env: Environment):
@@ -37,12 +45,18 @@ class CustomScheduler:
         """
         logger.info('creating CustomScheduler')
 
-        if cloud:
+        if consider_cloud_nodes:
             priorities: List[Tuple[float, Priority]] = [(1.0, BalancedResourcePriority()),
                                                         (1.0, LatencyAwareImageLocalityPriority()),
                                                         (1.0, CloudLocalityPriority()),
                                                         (1.0, DataLocalityPriority()),
                                                         (1.0, CapabilityPriority())]
+            #Add predicates to completely exclude edge node from scheduling
+            # predicates: List[Predicate] = [
+            #                                 PodFitsResourcesPred(),
+            #                                 CheckNodeLabelPresencePred(['data.skippy.io/storage'], False),
+            #                                 CheckCloudNodePred()
+            #                             ]
             return Scheduler(env.cluster,priorities=priorities)
         else:
             priorities: List[Tuple[float, Priority]] = [(1.0, BalancedResourcePriority()),
@@ -50,6 +64,12 @@ class CustomScheduler:
                                                         (1.0, EdgeLocalityPriority()),
                                                         (1.0, DataLocalityPriority()),
                                                         (1.0, CapabilityPriority())]
+            #Add predicates to completely exclude cloud node from scheduling
+            # predicates: List[Predicate] = [
+            #                                 PodFitsResourcesPred(),
+            #                                 CheckNodeLabelPresencePred(['data.skippy.io/storage'], False),
+            #                                 CheckEdgeNodePred()
+            #                             ]
             return Scheduler(env.cluster,priorities=priorities)
 
 
